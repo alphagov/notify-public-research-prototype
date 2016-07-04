3e3dad73ec211e0a198b4727e82c0a2dd2059177
Theodor Vararu <theodor.vararu@digital.cabinet-office.gov.uk>
2016-07-04 10:46:20 +0100

    Add deskpro, tawk, livechatinc routes

--- app/routes.js
+++ app/routes.js
@@ -5,29 +5,17 @@ router.get('/', function (req, res) {
   res.render('index')
 })
 
-// Example routes - feel free to delete these
-
-// Passing data into a page
-
-router.get('/examples/template-data', function (req, res) {
-  res.render('examples/template-data', { 'name': 'Foo' })
+router.get('/deskpro', function (req, res) {
+  console.log('deskpro')
+  res.render('index')
 })
 
-// Branching
-
-router.get('/examples/over-18', function (req, res) {
-  // get the answer from the query string (eg. ?over18=false)
-  var over18 = req.query.over18
-
-  if (over18 === 'false') {
-    // redirect to the relevant page
-    res.redirect('/examples/under-18')
-  } else {
-    // if over18 is any other value (or is missing) render the page requested
-    res.render('examples/over-18')
-  }
+router.get('/tawk', function (req, res) {
+  res.render('index')
 })
 
-// add your routes here
+router.get('/livechatinc', function (req, res) {
+  res.render('index')
+})
 
 module.exports = router
