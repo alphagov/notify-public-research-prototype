485d2f06961dd821f055f96fe27282971bcce790
Theodor Vararu <theodor.vararu@digital.cabinet-office.gov.uk>
2016-07-05 10:14:53 +0100

    Re-route non /overview routes to correct path for JSA and UC flows

--- app/routes.js
+++ app/routes.js
@@ -5,4 +5,12 @@ router.get('/', function (req, res) {
   res.render('index')
 })
 
+router.get('/jobseekers-allowance', function (req, res) {
+  res.render('jobseekers-allowance/overview')
+})
+
+router.get('/universal-credit', function (req, res) {
+  res.render('universal-credit/overview')
+})
+
 module.exports = router
