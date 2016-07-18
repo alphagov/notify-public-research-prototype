71eca6ef6d0f90e54f3d84d733026f7b7b5e2155
Theodor Vararu <theodor.vararu@digital.cabinet-office.gov.uk>
2016-07-18 14:50:54 +0100

    Add messages to conversation screen

--- app/components/Webchat.jsx
+++ app/components/Webchat.jsx
@@ -4,8 +4,25 @@ import WebchatConversation from './WebchatConversation'
 
 export default class Webchat extends Component {
   state = {
+    currentMessage: '',
+    messages: ['Hi, you\'re chatting with Ashan. How can I help you today?'],
     name: '',
     ready: false
+  }
+
+  handleMessageChange (currentMessage) {
+    this.setState({ currentMessage })
+  }
+
+  handleMessageSubmit (evt) {
+    const ENTER_KEY_CODE = 13
+    const isEnter = evt.keyCode === ENTER_KEY_CODE
+    if (isEnter) {
+      this.setState({
+        currentMessage: '',
+        messages: [...this.state.messages, this.state.currentMessage]
+      })
+    }
   }
 
   handleNameChange (name) {
@@ -30,8 +47,12 @@ export default class Webchat extends Component {
     }
     if (this.state.ready) {
       return <WebchatConversation
+        currentMessage={this.state.currentMessage}
         handleBack={this::this.changeToIntro}
+        handleMessageChange={this::this.handleMessageChange}
+        handleMessageSubmit={this::this.handleMessageSubmit}
         name={this.state.name}
+        messages={this.state.messages}
       />
     }
   }
--- app/components/WebchatConversation.jsx
+++ app/components/WebchatConversation.jsx
@@ -1,17 +1,32 @@
 import React, { Component, PropTypes } from 'react'
 import Button from './Button'
+import Input from './Input'
+import MessageList from './MessageList'
 
 export default class WebchatConversation extends Component {
   static propTypes = {
+    currentMessage: PropTypes.string.isRequired,
     handleBack: PropTypes.func.isRequired,
+    handleMessageChange: PropTypes.func.isRequired,
+    handleMessageSubmit: PropTypes.func.isRequired,
+    messages: PropTypes.arrayOf(PropTypes.string).isRequired,
     name: PropTypes.string.isRequired
   }
 
   render () {
     return <div>
-      Hello, {this.props.name}
+      <MessageList
+        messages={this.props.messages}
+      />
 
-      <Button onClick={this.props.handleBack}>Back to first screen (debug)</Button>
+      <Input
+        onChange={this.props.handleMessageChange}
+        onKeyUp={this.props.handleMessageSubmit}
+        type="text"
+        value={this.props.currentMessage}
+      />
+
+      <Button onClick={this.props.handleBack}>Back to first screen (debug, remove later)</Button>
     </div>
   }
 }
