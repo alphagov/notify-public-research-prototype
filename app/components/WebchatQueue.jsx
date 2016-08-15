d6cfb820033cc67274228b41c2f9a103a8c76027
Theodor Vararu <theodor.vararu@digital.cabinet-office.gov.uk>
2016-08-15 11:23:12 +0100

    Implement larger queue numbers in custom webchat

--- app/components/WebchatQueue.jsx
+++ app/components/WebchatQueue.jsx
@@ -3,6 +3,8 @@ import PropTypes from '../lib/PropTypes'
 import Button from './Button'
 import Dots from './Dots'
 
+const QUEUE_MAGNITUDE = 100
+
 export default class WebchatQueue extends Component {
   static propTypes = {
     handleQueueDecrement: PropTypes.func.isRequired,
@@ -13,15 +15,16 @@ export default class WebchatQueue extends Component {
   }
 
   render () {
-    const {queueSize} = this.props
+    const actualQueueSize = this.props.queueSize
+    const queueSize = Math.floor((actualQueueSize - 1) * QUEUE_MAGNITUDE + Math.random() * QUEUE_MAGNITUDE)
     if (this.props.isAgent) {
       if (this.props.userConnected) {
         return <div>
-          <p>The user is connected and is in a queue with {this.props.queueSize} {(queueSize !== 1) ? 'people' : 'person'} ahead.</p>
+          <p>The user is connected and is in a queue with between {(actualQueueSize - 1) * QUEUE_MAGNITUDE} to {actualQueueSize * QUEUE_MAGNITUDE} people ahead.</p>
           {(this.props.welcomeMessage) ? <p>Once the chat begins, the user will automatically receive this welcome message: "{this.props.welcomeMessage}"</p> : null}
           <p>To move them ahead, use the button below. When the count reaches 0, you'll both be taken to the chat.</p>
           <Button onClick={this.props.handleQueueDecrement}>
-            {(queueSize > 1) ? 'Decrement queue' : 'Begin chat'}
+            {(actualQueueSize > 1) ? 'Decrement queue by around 1000' : 'Begin chat'}
           </Button>
         </div>
       } else {
@@ -33,7 +36,7 @@ export default class WebchatQueue extends Component {
       return <div>
         <p className="mt5 tc">
           You are in a queue.<br />
-          There are {this.props.queueSize} people ahead of you.
+          There are {queueSize} people ahead of you.
         </p>
       </div>
     }
