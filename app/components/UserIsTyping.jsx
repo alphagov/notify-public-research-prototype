a05504e6c060f0738601a6e0fc37ecddd29f4e57
Theodor Vararu <theo@vararu.org>
2016-09-06 10:51:09 +0100

    Add aria label to typing notification

--- app/components/UserIsTyping.jsx
+++ app/components/UserIsTyping.jsx
@@ -7,7 +7,7 @@ export default class UserIsTyping extends Component {
   }
 
   render () {
-    return <div className="govuk-gray-1 f5" style={{ height: 0 }}>
+    return <div aria-label={`${this.props.user} is typing`} className="govuk-gray-1 f5" style={{ height: 0 }}>
       {this.props.user} is typing
     </div>
   }
